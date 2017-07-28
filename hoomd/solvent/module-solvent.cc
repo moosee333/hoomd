db4ef7b4886812cb41b3d2a66f1e26bf2ff181fd
Vyas <vramasub@umich.edu>
2017-07-28 18:02:32 -0400

    Updated modules file for CMake

--- hoomd/solvent/module-solvent.cc
+++ hoomd/solvent/module-solvent.cc
@@ -5,18 +5,27 @@
 // Maintainer: jglaser
 
 #include "GridData.h"
+#include "GridPotentialPair.h"
+#include "AllGridPairPotentials.h"
 
 //! Create the python module
 /*! each class setup their own python exports in a function export_ClassName
     create the hoomd python module and define the exports here.
 */
+
+using namespace solvent;
+
 PYBIND11_PLUGIN(_md)
     {
     pybind11::module m("_solvent");
 
     export_GridData(m);
+    export_GridPotentialPair<GridPotentialPairLJ>(m, "GridPotentialPairLJ");
+    export_GridPotentialPair<GridPotentialPairSLJ>(m, "GridPotentialPairSLJ");
+    export_GridPotentialPair<GridPotentialPairEwald>(m, "GridPotentialPairEwald");
+    export_GridPotentialPair<GridPotentialPairForceShiftedLJ>(m, "GridPotentialPairForceShiftedLJ");
     export_SnapshotGridData<float>(m);
     export_SnapshotGridData<double>(m);
 
-    return m.ptr;
+    return m.ptr();
     }
