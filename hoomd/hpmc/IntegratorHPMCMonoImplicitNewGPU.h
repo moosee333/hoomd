9c68df017bd7a440e5b549d84083308e802696b9
Jens Glaser <jsglaser@umich.edu>
2018-01-18 14:09:45 -0500

    tentative changes for avoiding illegal memory access

--- hoomd/hpmc/IntegratorHPMCMonoImplicitNewGPU.h
+++ hoomd/hpmc/IntegratorHPMCMonoImplicitNewGPU.h
@@ -1393,8 +1393,16 @@ void IntegratorHPMCMonoImplicitNewGPU< Shape >::updateCellWidth()
         m_cl_type[itype]->setNominalWidth(this->m_nominal_width);
         }
 
+    cudaDeviceSynchronize();
+    CHECK_CUDA_ERROR();
+
     // attach the parameters to the kernel stream so that they are visible
     // when other kernels are called
+    for (unsigned int i = 0; i < this->m_pdata->getNTypes(); ++i)
+        {
+        // attach nested memory regions
+        this->m_params[i].attach_to_stream(m_stream);
+        }
 
     cudaStreamAttachMemAsync(m_stream, this->m_params.data(), 0, cudaMemAttachSingle);
     CHECK_CUDA_ERROR();
@@ -1403,12 +1411,6 @@ void IntegratorHPMCMonoImplicitNewGPU< Shape >::updateCellWidth()
     cudaMemAdvise(this->m_params.data(), this->m_params.size()*sizeof(typename Shape::param_type), cudaMemAdviseSetReadMostly, 0);
     CHECK_CUDA_ERROR();
     #endif
-
-    for (unsigned int i = 0; i < this->m_pdata->getNTypes(); ++i)
-        {
-        // attach nested memory regions
-        this->m_params[i].attach_to_stream(m_stream);
-        }
     }
 
 
